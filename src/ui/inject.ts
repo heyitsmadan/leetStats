4fd265ebc5ea2a7f3d94982f9be196e1592f0ac8
Madan <heyitsmadan@gmail.com>
2025-06-27 11:08:37 +0530

    remove status div

--- src/ui/inject.ts
+++ src/ui/inject.ts
@@ -9,22 +9,22 @@ if (window.location.pathname.startsWith('/u/')) {
 
 function runAnalysis() {
     // Create a simple UI element to show progress
-    const statusDiv = document.createElement('div');
-    statusDiv.style.position = 'fixed';
-    statusDiv.style.bottom = '20px';
-    statusDiv.style.left = '20px';
-    statusDiv.style.padding = '10px 20px';
-    statusDiv.style.backgroundColor = '#282828';
-    statusDiv.style.color = '#fff';
-    statusDiv.style.borderRadius = '8px';
-    statusDiv.style.zIndex = '9999';
-    statusDiv.style.fontFamily = 'monospace';
-    statusDiv.textContent = 'LC Stats: Initializing...';
-    document.body.appendChild(statusDiv);
+    // const statusDiv = document.createElement('div');
+    // statusDiv.style.position = 'fixed';
+    // statusDiv.style.bottom = '20px';
+    // statusDiv.style.left = '20px';
+    // statusDiv.style.padding = '10px 20px';
+    // statusDiv.style.backgroundColor = '#282828';
+    // statusDiv.style.color = '#fff';
+    // statusDiv.style.borderRadius = '8px';
+    // statusDiv.style.zIndex = '9999';
+    // statusDiv.style.fontFamily = 'monospace';
+    // statusDiv.textContent = 'LC Stats: Initializing...';
+    // document.body.appendChild(statusDiv);
 
     const updateUICallback = (message: string) => {
         console.log(message);
-        statusDiv.textContent = `LC Stats: ${message}`;
+        // statusDiv.textContent = `LC Statgs: ${message}`;
     };
 
     // Kick off the main process
